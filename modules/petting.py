50c1bd4f08982d2e4fa6cd17b27e831318a1ac35
NobreHD <60514067+NobreHD@users.noreply.github.com>
2023-07-27 17:49:42 +0100

    This is annoying

--- modules/petting.py
+++ modules/petting.py
@@ -1,23 +1,16 @@
 from flask import jsonify
 import json, time
 
-file = "data/petting.json"
-saved = {}
+with open("data/petting.json", "r") as f:
+    saved = json.loads(f.read())
 last_request = 0
 
 def petting():
-    global last_request, saved
-    if time.time() - last_request > 60:
-        last_request = time.time()
-        with open(file, "r") as f:
-            saved = json.loads(f.read())
     return jsonify(saved)
 
 def setup(app):
     global saved
     app.add_url_rule('/petting', 'petting', petting, methods=['GET'])
-    with open(file, "r") as f:
-        saved = json.loads(f.read())
     print("Petting Routes Loaded")
 
 
