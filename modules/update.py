c2f1ff0578714fe3e86462498f027e076cf1abc8
NobreHD <60514067+NobreHD@users.noreply.github.com>
2023-07-27 17:42:54 +0100

    Test

--- modules/update.py
+++ modules/update.py
@@ -11,6 +11,7 @@ def update_thread():
     time.sleep(2)
     os.system("git pull")
     os.system("sudo systemctl kill -s HUP NobreAPI.service")
+    # test
 
 def update():
     signature = request.headers.get('x-hub-signature-256')
