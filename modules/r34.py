c32acd15769c587e056b41fbb284dc86610adde9
Leonardo Nobre <60514067+NobreHD@users.noreply.github.com>
2024-02-28 08:29:53 +0000

    Fix variable assignment in get_post_new function

--- modules/r34.py
+++ modules/r34.py
@@ -21,7 +21,7 @@ def get_post(post_id, streamer_mode):
     }
     
 def get_post_new(post_id):
-    r.scrape.get(f'https://rule34.xxx/index.php?page=dapi&s=post&q=index&id={post_id}&json=1')
+    r = scrape.get(f'https://rule34.xxx/index.php?page=dapi&s=post&q=index&id={post_id}&json=1')
     data = r.json()[0]
     source = list(filter(lambda x: validators.url(x), data['source'].split(' ')))
     source.insert(0, "https://rule34.xxx/index.php?page=post&s=view&id={post_id}")
